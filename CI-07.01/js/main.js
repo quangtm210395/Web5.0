b6a35625b8fcd888e1bf32c4aeaf342f8acfb0ef
Quang <quangtmse04402@fpt.edu.vn>
2017-01-07 22:41:10 +0700

    Fire

--- CI-07.01/js/main.js
+++ CI-07.01/js/main.js
@@ -1,6 +1,6 @@
-var Nakama = {};
-Nakama.configs = {
-}
+var Nakama = {
+};
+Nakama.configs = {};
 
 window.onload = function(){
   Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
@@ -23,16 +23,54 @@ var preload = function(){
   Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   Nakama.game.time.advancedTiming = true;
+
+  Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
+  Nakama.game.load.image('background', 'Assets/Map1.png');
 }
 
 // initialize the game
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
-  Nakama.keyboard = Nakama.game.input.keyboard; 
+  Nakama.keyboard = Nakama.game.input.keyboard;
+
+  Nakama.player = Nakama.game.add.sprite(
+    200,
+    200,
+    "assets",
+    "Spaceship1-Player.png"
+  );
+
+  Nakama.bulletCount = 0;
+  Nakama.bullets = [];
+
 }
 
 // update game state each frame
-var update = function(){}
+var update = function(){
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+    Nakama.player.position.y -= 10;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+    Nakama.player.position.y += 10;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+    Nakama.player.position.x -= 10;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+    Nakama.player.position.x += 10;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+    Nakama.bullets[Nakama.bulletCount++] = Nakama.game.add.sprite(
+      Nakama.player.position.x + 36 - 19,
+      Nakama.player.position.y -50,
+      "assets",
+      "BulletType1.png"
+    );
+  }
+  for(var i = 0; i < Nakama.bullets.length; i ++){
+    Nakama.bullets[i].position.y -= 10;
+  }
+}
 
 // before camera render (mostly for debug)
 var render = function(){}
