7a125090cc2113947fd4981e01f286f8526d1ffa
quangtm210395 <quangtmse04402@fpt.edu.vn>
2017-01-10 01:35:33 +0700

    bullets and fire

--- CI-07.01/js/main.js
+++ CI-07.01/js/main.js
@@ -1,6 +1,7 @@
 var Nakama = {};
 Nakama.configs = {};
 
+
 window.onload = function() {
     Nakama.game = new Phaser.Game(640, 960, Phaser.AUTO, '', {
         preload: preload,
@@ -27,46 +28,89 @@ var preload = function() {
 
 // initialize the game
 var create = function() {
+
     Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
     Nakama.keyboard = Nakama.game.input.keyboard;
 
+    Nakama.map = Nakama.game.add.tileSprite(0, 0, 640, 960, 'background');
+    Nakama.mapSpeed = 7;
+    Nakama.playerSpeed = 8;
+
     Nakama.player = Nakama.game.add.sprite(
-        200,
-        200,
+        Nakama.game.world.centerX - 36,
+        Nakama.game.world.centerY + 200,
         "assets",
         "Spaceship1-Player.png"
     );
+    Nakama.game.physics.arcade.enable(Nakama.player);
 
-    Nakama.bulletCount = 0;
-    Nakama.bullets = [];
+//  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+//setup bullets
+    Nakama.bulletTime = 0;
+    Nakama.bullets = Nakama.game.add.group();
+    Nakama.bullets.enableBody = true;
+    Nakama.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    Nakama.bullets.createMultiple(50, 'assets', "BulletType1.png");
+    Nakama.bullets.setAll('anchor.x', 0.5);
+    Nakama.bullets.setAll('anchor.y', 1);
+    Nakama.bullets.setAll('outOfBoundsKill', true);
+    Nakama.bullets.setAll('checkWorldBounds', true);
 
 }
 
 // update game state each frame
 var update = function() {
+
+
+  //scrolling map
+    Nakama.map.tilePosition.y += Nakama.mapSpeed;
+
     if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
-        Nakama.player.position.y -= 10;
+        if (Nakama.player.position.y >= Nakama.playerSpeed)
+            Nakama.player.position.y -= Nakama.playerSpeed;
+        else
+            Nakama.player.position.y = 0;
     }
     if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-        Nakama.player.position.y += 10;
+        if (Nakama.player.position.y <= (950 - 78))
+            Nakama.player.position.y += Nakama.playerSpeed;
+        else
+            Nakama.player.position.y = 960 - 78;
     }
     if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        Nakama.player.key = 'Spaceship1Left-Player.png';
-        Nakama.player.position.x -= 10;
+      Nakama.player.animations.add()
+        if (Nakama.player.position.x >= Nakama.playerSpeed)
+            Nakama.player.position.x -= Nakama.playerSpeed;
+        else
+            Nakama.player.position.x = 0;
     }
     if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        Nakama.player.position.x += 10;
+        if (Nakama.player.position.x <= (630 - 78))
+            Nakama.player.position.x += Nakama.playerSpeed;
+        else
+            Nakama.player.position.x = 640 - 78;
     }
     if (Nakama.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-        Nakama.bullets[Nakama.bulletCount++] = Nakama.game.add.sprite(
-            Nakama.player.position.x + 36 - 19,
-            Nakama.player.position.y - 50,
-            "assets",
-            "BulletType1.png"
-        );
+        fire();
     }
-    for (var i = 0; i < Nakama.bullets.length; i++) {
-        Nakama.bullets[i].position.y -= 30;
+
+
+}
+
+function fire() {
+
+    if (Nakama.game.time.now > Nakama.bulletTime) {
+
+        bullet = Nakama.bullets.getFirstExists(false);
+        if (bullet) {
+            bullet.reset(Nakama.player.position.x + 39 , Nakama.player.position.y);
+            bullet.body.velocity.y = -700;
+            Nakama.bulletTime = Nakama.game.time.now + 100;
+        }
     }
 }
 
